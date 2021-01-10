dcd80a44bf79b3e5757d51b9e8b8101a5d18c171
tamanna <tasminhaq27@gmail.com>
2021-01-09 21:49:25 -0500

    added wishlist page

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -6,7 +6,17 @@ import { Component } from '@angular/core';
   styleUrls: ['tab2.page.scss']
 })
 export class Tab2Page {
-
-  constructor() {}
+  productDetails: [{'productName': string, 'productPrice': string, 'productPic': string, 'productCount': string}];
+  constructor() {
+    // @ts-ignore
+    this.productDetails = [];
+    // tslint:disable-next-line:max-line-length
+    this.productDetails.push({productName: 'Microsoft Surface', productPrice: '$600', productPic: 'laptop-mssurface.png', productCount: '1'});
+    this.productDetails.push({productName: 'Acer Intel Celeron', productPrice: '$500', productPic: 'laptop-acer-celeron.jpg', productCount: '2'});
+    this.productDetails.push({productName: 'Lenovo Ideapad', productPrice: '$600', productPic: 'laptop-ideapad.jpg', productCount: '2'});
+    // tslint:disable-next-line:max-line-length
+    this.productDetails.push({productName: 'Microsoft Surface', productPrice: '$1000', productPic: 'laptop-mssurface.png', productCount: '2'});
+    this.productDetails.push({productName: 'Macbook pro', productPrice: '$1200', productPic: 'laptop-macbook.jpg', productCount: '2'});
+  }
 
 }
